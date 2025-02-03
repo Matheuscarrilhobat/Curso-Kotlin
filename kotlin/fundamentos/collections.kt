c2ff805c0501b2a6663a28914a168a12b9d22bac
Matheuscarrilhobat <155464159+Matheuscarrilhobat@users.noreply.github.com>
2025-02-03 14:59:55 -0300

    Add files via upload

--- kotlin/fundamentos/collections.kt
+++ kotlin/fundamentos/collections.kt
@@ -1,19 +1,53 @@
 package org.example.fundamentos
 
 fun main() {
-    var lista = mutableListOf(1,2,3,4,5,6)
 
-    println(lista)
+    var mapNomeIdade = mutableMapOf("Matheus" to 26,"Daniel" to 24)
+    println(mapNomeIdade)
 
-    lista.add(8)
-    lista.removeAt(0)
-    lista.remove(3)
+    mapNomeIdade["Bruno"] = 30
 
-    lista[0] = 20
+    println(mapNomeIdade)
 
-    println(lista)
+    mapNomeIdade.remove("Bruno")
 
+    println(mapNomeIdade)
 
+    mapNomeIdade.putIfAbsent("Bruno", 30) //adiciona valor apenas se não possuir o mesmo na lista
+
+    println(mapNomeIdade)
+
+    //var lista = mutableListOf(1,2,3,2)
+    //println("Lista: $lista")
+
+    //var setNumeros = mutableSetOf(1,2,3,2) //setOf não retorna valores duplicados
+
+    //println("Set: $setNumeros")
+
+    //println(setNumeros.contains(2))
+
+    //println(lista)
+
+    //lista.sort()
+
+    //println(lista)
+
+    //lista.shuffle()
+
+    //println(lista)
+
+    //var listaNomes = mutableListOf("Matheus","Gustavo")
+
+    //println(listaNomes)
+
+    //listaNomes.sort()
+
+    //println(listaNomes)
+
+    //lista.add(8)
+    //lista.removeAt(0)
+    //lista.remove(3)
+    //lista[0] = 20
 
     //var pares = lista.filter{ it % 2 == 0}.first() //first retorna apenas o primeiro numero par
     //lista.forEach{
