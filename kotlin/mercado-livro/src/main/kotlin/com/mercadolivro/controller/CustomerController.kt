02830169e8f06e0c69c15f8dd8e22e2b523806b4
Matheuscarrilhobat <155464159+Matheuscarrilhobat@users.noreply.github.com>
2025-02-18 14:37:13 -0300

    Add files via upload

--- kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
+++ kotlin/mercado-livro/src/main/kotlin/com/mercadolivro/controller/CustomerController.kt
@@ -1,14 +1,10 @@
 package com.mercadolivro.controller
 
 import com.mercadolivro.controller.request.PostCustomerRequest
+import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("customer")
@@ -17,7 +13,7 @@ class CustomerController {
     val customers = mutableListOf<CustomerModel>()
 
     @GetMapping
-    fun getCustomer(): MutableList<CustomerModel> {
+    fun getAll(): MutableList<CustomerModel> {
         return customers
     }
 
@@ -30,5 +26,19 @@ class CustomerController {
             customers.last().id.toInt() + 1
         }.toString()
         customers.add(CustomerModel(id,customer.name,customer.email))
+    }
+
+    @GetMapping("/{id}")
+    fun getCustomer(@PathVariable id: String): CustomerModel {
+        return customers.filter{ it.id == id}.first()
+    }
+
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun update(@PathVariable id: String, @RequestBody customer: PutCustomerRequest) {
+       customers.filter{ it.id == id}.first().let {
+           it.name = customer.name
+           it.email = customer.email
+       }
     }
 }
