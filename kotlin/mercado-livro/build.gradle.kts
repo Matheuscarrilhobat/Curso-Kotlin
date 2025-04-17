6bfc692e0ae10a7bdaab64f51dc4e7fc25374f9e
Matheuscarrilhobat <155464159+Matheuscarrilhobat@users.noreply.github.com>
2025-04-17 18:30:40 -0300

    Add files via upload

--- kotlin/mercado-livro/build.gradle.kts
+++ kotlin/mercado-livro/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.11.RELEASE"
 	kotlin("jvm") version "1.4.30"
 	kotlin("plugin.spring") version "1.4.30"
+	kotlin("plugin.jpa") version "1.4.30"
 }
 
 group = "com.mercadolivro"
@@ -21,8 +22,10 @@ dependencies {
 	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+	implementation("org.flywaydb:flyway-core:7.7.0")
 
-	runtimeOnly("mysql:mysql-connector-java")
+
+	runtimeOnly("mysql:mysql-connector-java:8.0.12")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
